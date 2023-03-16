200093b5f38ed5a3b3cedc39e24d4a274cf4de8a
Miguel Moreira <miguelmoreiracr7@gmail.com>
2023-03-15 21:51:13 -0300

    bloqueio de inserção de disciplinas com código igual

--- js/servico/disciplina-servico.js
+++ js/servico/disciplina-servico.js
@@ -5,11 +5,11 @@ class DisciplinaServico {
 
     inserir(codigo, nome) {
         const novaDisciplina = new Disciplina(codigo, nome);
-        console.log(novaDisciplina._codigo)
-        if(this.repositorio.disciplinas){
-            console.log("alo")
-            if(novaDisciplina._codigo === this.repositorio.disciplinas.filter(disciplina => disciplina._codigo === codigo)._codigo) {
+        if(this.repositorio._disciplinas[0]){
+            const verificar = this.repositorio._disciplinas.find(disciplina => disciplina._codigo === codigo);
+            if(verificar) {
                 console.log("código de disciplina inserido já existente, tente outro código");
+                return;
             }
             else {
                 this.repositorio.inserir(novaDisciplina);
